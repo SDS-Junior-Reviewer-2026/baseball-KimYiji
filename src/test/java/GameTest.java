9ab8b17ddf9bee201f42a8db1a9ac0fe1e069c66
KIMYIJI <yiji_kim@yonsei.ac.kr>
2026-04-03 13:39:07 +0900

    [feature] 38p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -22,4 +22,11 @@ public class GameTest {
             game.guess(null);
         });
     }
+
+    @Test
+    void throwExceptionWhenInputLengthIsUnmatched() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess("12");
+        });
+    }
 }
